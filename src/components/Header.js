5bda4e80d08e22f268011b2f0454a75aa0bbda25
Drew <iamdrew259@gmail.com>
2021-08-26 22:38:22 +0530

    Improve styling and more responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,26 @@
 import React, { Fragment } from "react";
 import styles from "./Header.module.css";
 import foodImage from "../assets/header-image.jpg";
-import chilliImage from "../assets/chilli-img.png";
+import chilliImage from "../assets/restro-app-logo.png";
 import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
   return (
     <Fragment>
       <header className={styles.header}>
-        <h1 className={styles["text-header"]}>The Spicy Saga</h1>
-        {/* <h1 className={styles["img-header"]}>
-          <img src={chilliImage} alt="Spicy icon" />
-          Saga
-        </h1> */}
+        <h1 className={`${styles["text-header"]} ${styles["larger-screen"]}`}>
+          The Spicy Saga
+        </h1>
+        <h1 className={styles["img-header"]}>
+          <span>
+            <img
+              className={styles["logo-img"]}
+              src={chilliImage}
+              alt="Spicy logo"
+            />
+          </span>
+          <span className={styles["text-header"]}>Saga</span>
+        </h1>
         <HeaderCartButton />
       </header>
       <div>
